58c0f05855752374fae04dbb33355897e85ae1fc
Student <student@epicodus-7.hsd1.or.comcast.net>
2022-11-17 15:56:47 -0800

    updated code from Chris's rpg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+// import Player from "../src/js/players.js";
+import Monster from "../src/js/monster";
+
+// let newPlayer = new Player(100, 0, 0);
+let newMonst = new Monster(200);
+
+function handleWarrior(event) {
+	event.preventDefault();
+
+	document.getElementById("hp-stat").innerHTML = 100;
+	document.getElementById("strength-stat").innerHTML = 90;
+	document.getElementById("magic-stat").innerHTML = 40;
+	document.getElementById("monster-hp").innerHTML = newMonst.hp;
+
+	document.getElementById("warrior-card").classList.add("invisible");
+	document.getElementById("druid-card").classList.add("invisible");
+	document.getElementById("mage-card").classList.add("invisible");
+	document.getElementById("hunter-card").classList.add("invisible");
+	document.getElementById("stats").classList.remove("invisible");
+}
+
+function handleDruid(event) {
+	event.preventDefault();
+	document.getElementById("hp-stat").innerHTML = 100;
+	document.getElementById("strength-stat").innerHTML = 80;
+	document.getElementById("magic-stat").innerHTML = 80;
+	document.getElementById("monster-hp").innerHTML = newMonst.hp;
+
+	document.getElementById("warrior-card").classList.add("invisible");
+	document.getElementById("druid-card").classList.add("invisible");
+	document.getElementById("mage-card").classList.add("invisible");
+	document.getElementById("hunter-card").classList.add("invisible");
+	document.getElementById("stats").classList.remove("invisible");
+}
+
+function handleMage(event) {
+	event.preventDefault();
+
+	document.getElementById("hp-stat").innerHTML = 100;
+	document.getElementById("strength-stat").innerHTML = 40;
+	document.getElementById("magic-stat").innerHTML = 90;
+	document.getElementById("monster-hp").innerHTML = newMonst.hp;
+
+	document.getElementById("warrior-card").classList.add("invisible");
+	document.getElementById("druid-card").classList.add("invisible");
+	document.getElementById("mage-card").classList.add("invisible");
+	document.getElementById("hunter-card").classList.add("invisible");
+	document.getElementById("stats").classList.remove("invisible");
+}
+
+function handleHunter(event) {
+	event.preventDefault();
+
+	document.getElementById("hp-stat").innerHTML = 100;
+	document.getElementById("strength-stat").innerHTML = 70;
+	document.getElementById("magic-stat").innerHTML = 70;
+	document.getElementById("monster-hp").innerHTML = newMonst.hp;
+
+	document.getElementById("warrior-card").classList.add("invisible");
+	document.getElementById("druid-card").classList.add("invisible");
+	document.getElementById("mage-card").classList.add("invisible");
+	document.getElementById("hunter-card").classList.add("invisible");
+	document.getElementById("stats").classList.remove("invisible");
+}
+function handleAttack(event) {
+	event.preventDefault();
+	if(newMonst.isDead()) {
+		document.getElementById("dead").innerHTML = "You Have Won The Battle!!!";
+	} else {
+		newMonst.attack();
+	document.getElementById("monster-hp").innerHTML = newMonst.hp;
+	}
+}
+
+window.addEventListener("load", function () {
+	document.getElementById("warrior").addEventListener("submit", handleWarrior);
+	document.getElementById("druid").addEventListener("submit", handleDruid);
+	document.getElementById("mage").addEventListener("submit", handleMage);
+	document.getElementById("hunter").addEventListener("submit", handleHunter);
+	document.getElementById("attack").addEventListener("submit", handleAttack);
+});
--- src/js/monster.js
+++ src/js/monster.js
@@ -3,13 +3,38 @@ export default class Monster {
     this.hp = hp;
   }
 
+
   monsterCrit() {
     let critHit = 25;
     this.hp -= critHit;
   }
 
-  monsterRegHit(){
+  monsterRegHit() {
     let regHit = 10;
     this.hp -= regHit;
   }
-}
+
+  randomNum() {
+    let roll = Math.floor(Math.random() * (100) + 1);
+    if (roll <= 100) {
+      return roll;
+    }
+  }
+
+  attack() {
+    let num = Math.floor(Math.random() * (100) + 1);
+    if (this.hp <= 0) {
+      return "You won!";
+    } else if (num <= 60) {
+      this.hp -= 10;
+    } else {
+      this.hp -= 25;
+    }
+  }
+  isDead() {
+    if (this.hp <= 0) {
+      return "you won";
+    }
+
+  }
+} 
